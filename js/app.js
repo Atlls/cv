1fb6cdfdbaa4e171af0ea0543ae137752424d22a
unknown <alejandroaigner1999@hotmail.com>
2021-04-05 14:15:57 -0400

    Normalizar

--- js/app.js
+++ js/app.js
@@ -1,27 +1,27 @@
 'use strict';
 
-async function inyectarPDF(){
+// async function inyectarPDF(){
 
-	const iframe = await iframeLoaded('docs/Alejandro aigner (2).pdf');
+// 	const iframe = await iframeLoaded('docs/Alejandro aigner (2).pdf');
 
-	console.log('Cargado el PDF');
+// 	console.log('Cargado el PDF');
 
-	const spinner = document.querySelector('#spinner');
-	document.body.replaceChild(iframe,spinner);
+// 	const spinner = document.querySelector('#spinner');
+// 	document.body.replaceChild(iframe,spinner);
 
-}
+// }
 
-function iframeLoaded(src) {
+// function iframeLoaded(src) {
 
-    return new Promise ( resolve => 
-    {
-        const iframe = document.createElement('iframe');
-		iframe.setAttribute('src',src);
-		iframe.setAttribute('frameborder','0');
-		console.log(iframe);
+//     return new Promise ( resolve => 
+//     {
+//         const iframe = document.createElement('iframe');
+// 		iframe.setAttribute('src',src);
+// 		iframe.setAttribute('frameborder','0');
+// 		console.log(iframe);
 
-        iframe.addEventListener('load',resolve(iframe));
-    });
-}
+//         iframe.addEventListener('load',resolve(iframe));
+//     });
+// }
 
-inyectarPDF();
+// inyectarPDF();
